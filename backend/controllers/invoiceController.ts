a3643a38b38a234443112d42f4f8ef0bd6dab010
vitthal4545 <vitthalgs1434@gmail.com>
2026-01-15 23:34:04 +0530

    feat(backend): add create invoice endpoint

---
+++ backend/controllers/invoiceController.ts
@@ -0,0 +1,54 @@
+import { Request, Response } from "express";
+import Invoice, { IItem } from "../models/Invoice.js";
+
+export const createInvoice = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+
+    const {
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+    } = req.body;
+
+    let subTotal = 0;
+    let taxTotal = 0;
+    items.forEach((item: IItem) => {
+      subTotal += item.unitPrice * item.quantity;
+      taxTotal +=
+        (item.unitPrice * item.quantity * (item.taxPercent || 0)) / 100;
+    });
+
+    const total = subTotal + taxTotal;
+
+    const invoice = new Invoice({
+      user,
+      invoiceNumber,
+      invoiceDate,
+      dueDate,
+      billFrom,
+      billTo,
+      items,
+      notes,
+      paymentTerms,
+      subTotal,
+      taxTotal,
+      total,
+    });
+
+    await invoice.save();
+
+    res.status(200).json(invoice);
+  } catch (error) {
+    if (error instanceof Error) {
+      res
+        .status(500)
+        .json({ message: "Error creating invoice", error: error.message });
+    }
+  }
+};
---
+++ backend/routes/invoiceRoutes.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createInvoice,
+  getInvoices,
+  getInvoiceById,
+  updateInvoice,
+  deleteInvoice,
+} from "../controllers/invoiceController.js";
+
+import protect from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").post(protect, createInvoice);
+
+export default router;

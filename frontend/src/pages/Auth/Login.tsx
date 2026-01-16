fdb96e541f465986c32fd2dfd93eaf80272ed1f3
vitthal4545 <vitthalgs1434@gmail.com>
2026-01-16 23:35:29 +0530

    feat(frontend): add login page UI

---
+++ frontend/src/pages/Auth/Login.tsx
@@ -0,0 +1,182 @@
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import React, { useState } from "react";
+import {
+  ArrowRight,
+  Eye,
+  EyeOff,
+  FileText,
+  Loader2,
+  Lock,
+  Mail,
+} from "lucide-react";
+import { validateEmail, validatePassword } from "../../utils/helper";
+import axiosInstance from "../../utils/axiosInstance";
+import { API_PATHS } from "../../utils/apiPath";
+
+// types
+type FieldName = "email" | "password";
+
+const Login = () => {
+  const { login } = useAuth();
+  const navigate = useNavigate();
+
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [success, setSuccess] = useState("");
+  const [fieldErrors, setFieldErrors] = useState({
+    email: "",
+    password: "",
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
+  });
+
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center px-4">
+      <div className="w-full max-w-sm">
+        <div className="text-center mb-8 ">
+          <div className="w-12 h-12 bg-gradient-to-r from-blue-950 to-blue-900 rounded-xl mx-auto mb-6 flex items-center justify-center">
+            <FileText className="w-6 h-6 text-white" />
+          </div>
+          <h1 className="text-2xl font-semibold text-gray-900 mb-2">
+            Login to Your Account
+          </h1>
+          <p className="text-gray-600 text-sm">
+            Welcome back to Invoice Generator
+          </p>
+        </div>
+
+        <div className="space-y-4">
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Email
+            </label>
+            <div className="relative">
+              <Mail className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-5 h-5" />
+            </div>
+
+            <input
+              name="email"
+              type="email"
+              required
+              value={formData.email}
+              onChange={handleInputChange}
+              onBlur={handleBlur}
+              className={`w-full pl-12 pr-4 py-3 border rounded-lg focus:ring-2 focus:border-transparent outline-none transition-all ${
+                fieldErrors.email && touched.email
+                  ? "border-red-300 focus:ring-red-500"
+                  : "border-gray-300 focus:ring-black"
+              }`}
+              placeholder="Enter your email"
+            />
+          </div>
+          {fieldErrors.email && touched.email && (
+            <p className="mt-1 text-sm text-red-600">{fieldErrors.email}</p>
+          )}
+
+          <div>
+            <label className="block text-sm font-medium text-gray-700 mb-2">
+              Password
+            </label>
+
+            <div className="relative">
+              {/* Lock icon */}
+              <Lock className="absolute left-4 top-1/2 -translate-y-1/2 text-gray-400 w-5 h-5" />
+
+              {/* Input */}
+              <input
+                name="password"
+                type={showPassword ? "text" : "password"}
+                required
+                value={formData.password}
+                onChange={handleInputChange}
+                onBlur={handleBlur}
+                className={`w-full pl-12 pr-12 py-3 border rounded-lg focus:ring-2 focus:border-transparent outline-none transition-all ${
+                  fieldErrors.password && touched.password
+                    ? "border-red-300 focus:border-red-500"
+                    : "border-gray-300 focus:ring-black"
+                }`}
+                placeholder="Enter your password"
+              />
+
+              {/* Eye button */}
+              <button
+                type="button"
+                onClick={() => setShowPassword((prev) => !prev)}
+                className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 transition-colors"
+              >
+                {showPassword ? (
+                  <EyeOff className="w-5 h-5" />
+                ) : (
+                  <Eye className="w-5 h-5" />
+                )}
+              </button>
+            </div>
+
+            {fieldErrors.password && touched.password && (
+              <p className="mt-1 text-sm text-red-600">
+                {fieldErrors.password}
+              </p>
+            )}
+          </div>
+
+          {/* {fieldErrors.password && touched.password && (
+            <p className="mt-1 text-sm text-red-600">{fieldErrors.password}</p>
+          )} */}
+
+          {error && (
+            <div className="p-3 bg-red-50 border border-red-200 rounded-lg">
+              <p className="text-red-600 text-sm">{error}</p>
+            </div>
+          )}
+
+          {success && (
+            <div className="p-3 bg-green-50 border border-green-200 rounded-lg">
+              <p className="text-green-600 text-sm">{success}</p>
+            </div>
+          )}
+
+          <button
+            onClick={handleSubmit}
+            disabled={isLoading || !isFormValid()}
+            className="w-full bg-gradient-to-r from-blue-950 to-blue-900 text-white py-3 px-4 rounded-lg font-medium hover:bg-gray-800 disabled:bg-gray-300 disabled:cursor-not-allowed transition-colors flex items-center justify-center group"
+          >
+            {isLoading ? (
+              <>
+                <Loader2 className="w-4 h-4 mr-2 animate-spin" />
+                Signing in...
+              </>
+            ) : (
+              <>
+                Sign in
+                <ArrowRight className="w-4 h-4 ml-2 group-hover:translate-x-1 transition-transform" />
+              </>
+            )}
+          </button>
+        </div>
+
+        <div className="mt-6 pt-4 border-t border-gray-200 text-center">
+          <p className="text-sm text-gray-600">
+            Don't have an account?{" "}
+            <button
+              className="text-black font-medium hover:underline cursor-pointer"
+              onClick={() => navigate("/signup")}
+            >
+              Signup
+            </button>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
